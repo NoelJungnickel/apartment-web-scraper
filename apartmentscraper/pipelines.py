6e10bfd610b2c66a65004c962da1b8e1c3780387
nulli <n.jungnickel@campus.tu-berlin.de>
2024-04-03 01:15:04 +0200

    finished first spider and added pipeline

--- apartmentscraper/pipelines.py
+++ apartmentscraper/pipelines.py
@@ -1,13 +1,35 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pandas
 
 
 class ApartmentscraperPipeline:
+
+    items = []
+
+    slots = [
+        "Location in the object",
+        "Apartment number",
+        "Room",
+        "Living space (in m²)",
+        "Total purchase price",
+        "Status",
+    ]
+
+    def close_spider(self, spider):
+        df = pandas.DataFrame(list(item["apartment"] for item in self.items), columns = self.slots, index=list(range(1, len(self.items) + 1)))
+        df.to_excel("apartments.xlsx")
+
     def process_item(self, item, spider):
+
+        if len(item["apartment"]) < 2:
+            return
+        
+        item["apartment"] = item["apartment"][1:]
+
+        item["apartment"][3] = item["apartment"][3].replace(",", ".")
+        item["apartment"][4] = item["apartment"][4].replace(" ", "").replace("€", " €").replace(",", ".")
+
+
+        self.items.append(item)
+
         return item
--- apartmentscraper/spiders/apartments_spider.py
+++ apartmentscraper/spiders/apartments_spider.py
@@ -11,15 +11,15 @@ class ApartmentsSpider(scrapy.Spider):
     def parse(self, response):
         apartments = response.css("div.elementor")
 
-        for apartment in apartments:
-            yield {
-                "location": apartment.css("td::text").getall(),
-                "number": apartment.css("td::text").getall(),
-                "rooms": apartment.css("td::text").getall(),
-                "size": apartment.css("td::text").getall(),
-                "price": apartment.css("td::text").getall(),
-                "status": apartment.css("td::text").getall(),
-            }
+        #for apartment in apartments:
+        #    yield {
+        #        "location": apartment.css("td::text").getall(),
+        #        "number": apartment.css("td::text").getall(),
+        #        "rooms": apartment.css("td::text").getall(),
+        #        "size": apartment.css("td::text").getall(),
+        #        "price": apartment.css("td::text").getall(),
+        #        "status": apartment.css("td::text").getall(),
+        #    }
 
         #apartment_data = ApartmentscraperItem()
         #apartment_data["location"] = response.css("td::text").getall(),
@@ -31,7 +31,7 @@ class ApartmentsSpider(scrapy.Spider):
 
         #yield apartment_data
 
-        #for apartment in response.css("div.elementor"):
-        #    yield {
-        #        "td": apartment.css("td::text").getall(),
-        #    }
+        for apartment in response.css("tr"):
+            yield {
+                "apartment": apartment.css(" td::text").extract(),
+            }
